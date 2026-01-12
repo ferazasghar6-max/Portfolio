7d848d2b59e10e2b04064a0bca26bb9bf6716620
ferazasghar6-max <ferazasghar6@gmail.com>
2026-01-12 21:39:13 +0100

    Say-Hi-component / Inputarea validation partially completed

--- src/app/components/say-hi-component/say-hi-component.ts
+++ src/app/components/say-hi-component/say-hi-component.ts
@@ -1,21 +1,54 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-say-hi-component',
+  standalone: true,
   imports: [FormsModule],
   templateUrl: './say-hi-component.html',
   styleUrl: './say-hi-component.scss',
 })
 export class SayHiComponent {
 
+  http = inject(HttpClient);
+
   contactData = {
     name: "",
     email: "",
     message: "",
+    privacyPolicy: "",
   }
 
-  onSubmit(){
-    console.log(this.contactData)
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
   }
 }

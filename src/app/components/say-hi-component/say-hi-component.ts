ecc76db19730f94787461c83c5420c2fea85f244
ferazasghar6-max <ferazasghar6@gmail.com>
2026-01-12 13:27:47 +0100

    try to fix ngModal

--- src/app/components/say-hi-component/say-hi-component.ts
+++ src/app/components/say-hi-component/say-hi-component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-say-hi-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './say-hi-component.html',
   styleUrl: './say-hi-component.scss',
 })
 export class SayHiComponent {
 
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  onSubmit(){
+    console.log(this.contactData)
+  }
 }

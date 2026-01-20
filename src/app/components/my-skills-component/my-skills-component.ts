19e4af268f8732ba7911c9b82327d57a9ff5118c
ferazasghar6-max <ferazasghar6@gmail.com>
2026-01-20 11:48:27 +0100

    responsivness in progress

--- src/app/components/my-skills-component/my-skills-component.ts
+++ src/app/components/my-skills-component/my-skills-component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-my-skills-component',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './my-skills-component.html',
   styleUrl: './my-skills-component.scss',
 })
+
 export class MySkillsComponent {
 
 }

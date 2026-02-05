0efb359a9a3276a69be2a3ee2c69710e20bcc8b9
ferazasghar6-max <ferazasghar6@gmail.com>
2026-02-05 13:29:42 +0100

    Inputfiled completet --> required disabled button + input-background

--- src/app/components/say-hi-component/say-hi-component.ts
+++ src/app/components/say-hi-component/say-hi-component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-say-hi-component',
@@ -11,15 +11,14 @@ import { RouterLink } from "@angular/router";
   styleUrl: './say-hi-component.scss',
 })
 export class SayHiComponent {
-
   http = inject(HttpClient);
 
   contactData = {
-    name: "",
-    email: "",
-    message: "",
-    privacyPolicy: "",
-  }
+    name: '',
+    email: '',
+    message: '',
+    privacyPolicy: '',
+  };
 
   mailTest = true;
 
@@ -35,21 +34,30 @@ export class SayHiComponent {
   };
 
   onSubmit(ngForm: NgForm) {
-    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
-      this.http.post(this.post.endPoint, this.post.body(this.contactData))
-        .subscribe({
-          next: (response) => {
-
-            ngForm.resetForm();
-          },
-          error: (error) => {
-            console.error(error);
-          },
-          complete: () => console.info('send post complete'),
-        });
-    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
-
-      ngForm.resetForm();
+    // Check if form is valid first
+    if (ngForm.valid) {
+      if (!this.mailTest) {
+        this.http
+          .post(this.post.endPoint, this.post.body(this.contactData), this.post.options)
+          .subscribe({
+            next: (response) => {
+              ngForm.resetForm();
+            },
+            error: (error) => {
+              console.error(error);
+            },
+            complete: () => console.info('send post complete'),
+          });
+      } else {
+        // Logic for MailTest = true
+        console.log('Form is valid, but in test mode:', this.contactData);
+        ngForm.resetForm();
+      }
+    } else {
+      // If form is invalid, this block executes.
+      // The HTML will now show errors because ngForm.submitted is true.
+      console.log('Form is invalid');
     }
   }
+
 }

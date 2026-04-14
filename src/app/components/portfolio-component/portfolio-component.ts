a9c1d83d67215ef0ad54c38fbb8e63053df1993a
ferazasghar6-max <ferazasghar6@gmail.com>
2026-04-15 00:32:57 +0200

    animate on scroll completed

--- src/app/components/portfolio-component/portfolio-component.ts
+++ src/app/components/portfolio-component/portfolio-component.ts
@@ -8,28 +8,5 @@ import { TranslatePipe } from '@ngx-translate/core';
   styleUrl: './portfolio-component.scss',
 })
 export class PortfolioComponent {
-  constructor() {}
 
-  ngOnInit(): void {
-    // Hier rufen wir die Methode auf, wenn die Komponente geladen wird
-    this.initPortfolioScroll();
-  }
-
-  initPortfolioScroll(): void {
-    if (window.innerWidth >= 1024) return; // Early Return: Tu nichts, wenn Desktop
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          // toggleClass fügt hinzu wenn im Bild, entfernt wenn draußen
-          entry.target.classList.toggle('is-visible', entry.isIntersecting);
-        });
-      },
-      { threshold: 0.8, rootMargin: '10% 0px 10% 0px' },
-    );
-
-    document
-      .querySelectorAll('.left-content, .right-content')
-      .forEach((el) => observer.observe(el));
-  }
 }
